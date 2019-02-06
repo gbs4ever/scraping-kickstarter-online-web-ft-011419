c08f6fac16ac27402814369f2529135a91a75074
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-06 18:15:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,20 +3,10 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
+  html = File.read('https://www.bestbuy.com/site/misc/deal-of-the-day/pcmcat248000050016.c?id=pcmcat248000050016')
 
-  kickstarter = Nokogiri::HTML(html)
-  var=kickstarter.css("li.project.grid_4") # write your code here
-projects={}
-var.each do |project|
-  title = project.css("h2.bbcard_name strong a").text
-projects[title.to_sym] = {
-:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
- :description =>  project.css("p.bbcard_blurb").text,
-:location=> project.css("ul.project-meta span.location-name").text,
-:percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-}
-end
-projects
+file  = Nokogiri::HTML(html)
+binding.pry
+
 
 end
